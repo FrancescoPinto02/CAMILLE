c2783657d88cd2e38e379ec5390ec0ee8f4c6a32
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-05-10 22:52:02 +0200

    feat: db creation script

---
+++ db/create.sql
@@ -0,0 +1,54 @@
+DROP SCHEMA IF EXISTS camille_db;
+CREATE DATABASE IF NOT EXISTS camille_db;
+USE camille_db;
+
+CREATE TABLE pipeline_stage (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE effect (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE codesmell (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    type ENUM('Generic', 'API-Specific') NOT NULL,
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE codesmell_stage (
+    codesmell_id INT NOT NULL,
+    stage_id INT NOT NULL,
+    
+    FOREIGN KEY (codesmell_id) REFERENCES codesmell(id) 
+		ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (stage_id) REFERENCES pipeline_stage(id) 
+		ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    PRIMARY KEY (codesmell_id, stage_id)
+);
+
+CREATE TABLE codesmell_effect (
+    codesmell_id INT NOT NULL,
+    effect_id INT NOT NULL,
+    
+    FOREIGN KEY (codesmell_id) REFERENCES codesmell(id) 
+		ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    FOREIGN KEY (effect_id) REFERENCES effect(id) 
+		ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    PRIMARY KEY (codesmell_id, effect_id)
+);
