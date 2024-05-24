b628533eea0ea83d5b0c804a87362a54260999d3
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-05-24 22:14:54 +0200

    feat: specific code smell info with entities

--- actions/actions.py
+++ actions/actions.py
@@ -46,7 +46,55 @@ class ActionGetCodeSmellsList(Action):
             except mysql.connector.Error as err:
                 dispatcher.utter_message(text=f"Database error: {err}")
             finally:
-                connection.close() # Closing Connection
+                connection.close()  # Closing Connection
+        else:
+            dispatcher.utter_message(text="Failed to connect to the database.")
+
+        return []
+
+
+class ActionProvideCodeSmellDetails(Action):
+
+    def name(self) -> Text:
+        return "action_provide_code_smell_details"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        code_smell_id = next(tracker.get_latest_entity_values("code_smell_id"), None)
+        code_smell_name = next(tracker.get_latest_entity_values("code_smell_name"), None)
+
+        if not code_smell_id and not code_smell_name:
+            dispatcher.utter_message(text="I'm sorry, I didn't understand what code smell you were referring to.")
+            return []
+
+        connection = get_connection()
+        if connection:
+            try:
+                cursor = connection.cursor()
+
+                if code_smell_id:
+                    query = "SELECT description FROM codesmell WHERE id=%s"
+                    cursor.execute(query, (code_smell_id,))
+                    result = cursor.fetchone()
+                else:
+                    query = "SELECT description FROM codesmell WHERE name=%s"
+                    cursor.execute(query, (code_smell_name,))
+                    result = cursor.fetchone()
+
+                if result:
+                    code_smell_details = result[0]
+                    dispatcher.utter_message(text=f"{code_smell_details}")
+                else:
+                    dispatcher.utter_message(text="Sorry, I couldn't find any details about that.")
+
+                cursor.close()
+
+            except mysql.connector.Error as err:
+                dispatcher.utter_message(text=f"Database error: {err}")
+            finally:
+                connection.close()  # Closing Connection
         else:
             dispatcher.utter_message(text="Failed to connect to the database.")
 
