f1a8f7d34ae0ec771e2fa9aee679e9d5f0b53644
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-07-22 19:05:22 +0200

    added requirements

--- actions/action_default_fallback.py
+++ actions/action_default_fallback.py
@@ -2,6 +2,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+
 class ActionDefaultFallback(Action):
 
     def name(self) -> Text:
